a5cf7ae53b28c41167ed3945ab4ee03892cdf5a6
anushkasenmehar1208-hub <anushkasenmehar1208@gmail.com>
2026-03-03 01:52:45 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,6 @@ import os
 
 config = rx.Config(
     app_name="uni_app",
-    api_url=f"https://{os.getenv('RAILWAY_PUBLIC_DOMAIN')}",
-    db_url=os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_l2RpcO8fGLtu@ep-long-feather-a1d7txg7.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"),
+    api_url="https://uniapp-production-01d0.up.railway.app",
+    db_url=os.getenv("DATABASE_URL"),
 )
