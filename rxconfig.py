ce7d77827171ddb96faa9ca7162aed2ee239f514
anushkasenmehar1208-hub <anushkasenmehar1208@gmail.com>
2026-03-05 20:40:35 +0530

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -6,9 +6,12 @@ default_api_url = "https://alexstudies.com"
 
 config = rx.Config(
     app_name="uni_app",
-    api_url=os.getenv("API_URL", default_api_url),
-    db_url=os.getenv("DATABASE_URL", "sqlite:///reflex.db"),
+    api_url="https://alexstudies.com",
+    db_url="sqlite:///reflex.db",
     frontend_port=3001,
     backend_port=8000,
     show_built_with_reflex=False,
+    plugins=[
+        "reflex.plugins.sitemap.SitemapPlugin",
+    ],
 )
