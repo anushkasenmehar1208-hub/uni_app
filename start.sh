198071db05c4a802fd80df13da02654cf447346c
anushkasenmehar1208-hub <anushkasenmehar1208@gmail.com>
2026-03-07 20:35:50 +0530

    asdfghjkl

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 reflex export --frontend-only --no-zip
-reflex run --env prod --frontend-port 3001 --backend-port 8000 &
+reflex run --env prod --frontend-port 3002 --backend-port 8000 &
 caddy run --config /app/Caddyfile --adapter caddyfile
