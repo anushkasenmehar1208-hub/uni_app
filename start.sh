8caeb4bce7c49a2cdde6e4e29f31d1cea7352a7d
anushkasenmehar1208-hub <anushkasenmehar1208@gmail.com>
2026-03-07 19:56:48 +0530

    zxcvbnm

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-set -e
-
 reflex export --frontend-only --no-zip
-reflex run --env prod --backend-only --backend-host 127.0.0.1 --backend-port 8000 &
+reflex run --env prod --frontend-port 3001 --backend-port 8000 &
 caddy run --config /app/Caddyfile --adapter caddyfile
