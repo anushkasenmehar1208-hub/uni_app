311a5d3566a46c1fc8091e5c424b09793477847e
anushkasenmehar1208-hub <anushkasenmehar1208@gmail.com>
2026-03-09 14:40:01 +0530

    fhj

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@ set -e
 
 APP_PORT="${PORT:-8080}"
 
-# Run Reflex on Railway's assigned public port (single process, no Caddy proxy layer).
-reflex run --env prod --single-port --frontend-port "${APP_PORT}" --backend-port "${APP_PORT}"
+# Run migrations first
+reflex db migrate
+
+# Run the app once using the correct port
+reflex run --env prod --single-port --frontend-port "$APP_PORT" --backend-port "$APP_PORT"
