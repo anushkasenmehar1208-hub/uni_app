073f3e3cbb1ce3ab7e62d14353a21d8eb1cbb0a5
anushkasenmehar1208-hub <anushkasenmehar1208@gmail.com>
2026-03-10 18:01:13 +0530

    jj

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 APP_PORT="${PORT:-8080}"
 
 # Run migrations first
-reflex db makemigrations --noqa && reflex db migrate
+reflex db makemigrations && reflex db migrate
 
 # Run the app once using the correct port
 reflex run --env prod --single-port --frontend-port "$APP_PORT" --backend-port "$APP_PORT"
